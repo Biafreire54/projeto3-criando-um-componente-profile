6b7c08afdafd35ff9240e8471750b6917864d13b
Ana Beatriz Freire <160136677+Biafreire54@users.noreply.github.com>
2024-11-06 12:49:47 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Profile from './components/Profile'
+
+export default function App() {
+  return( 
+        <div>
+          <Profile 
+           img={'./bia.jpeg'}
+           name={'Ana Beatriz Freire Ramos'} 
+           bio={'Estudante de Desenvolvimento de Sistemas'} 
+           contact={'+ 55 (85) 998736-8938'} 
+           email={'Bia.freire2204@gmail.com'} 
+           githubUrl={'https://github.com/Biafreire54'} 
+           instagramUrl={'https://instagram.com/y_beatriz56'} 
+           vercelUrl={'https://vercel.com/biafreire54s-projects'}
+           />
+        </div> 
+   
+  )
+}
